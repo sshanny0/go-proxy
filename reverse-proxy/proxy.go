d5d7b65a8c7883fdcaebbbb7b3b4832a3fc18da0
unknown <shfhanny@gmail.com>
2026-04-07 15:17:15 +0700

    fix: load balancer with minimal memory

--- reverse-proxy/proxy.go
+++ reverse-proxy/proxy.go
@@ -9,42 +9,78 @@ import (
 	"time"
 )
 
+type Backend struct {
+	URL   *url.URL
+	Alive bool
+}
+
 type ReverseProxy struct {
-	backends []*url.URL
+	backends []*Backend
 	current  uint64
 }
 
 func NewReverseProxy(backendsURLs []string) (*ReverseProxy, error) {
-	urls := make([]*url.URL, len(backendsURLs))
+	backends := make([]*Backend, len(backendsURLs))
+
 	for i, u := range backendsURLs {
 		parsedURL, err := url.Parse(u)
 		if err != nil {
 			return nil, err
 		}
-		urls[i] = parsedURL
+
+		backends[i] = &Backend{
+			URL:   parsedURL,
+			Alive: true,
+		}
 	}
+
 	return &ReverseProxy{
-		backends: urls,
+		backends: backends,
 	}, nil
 }
 
+// BACKEND SLEECTOR TO SKIP UNHEALTHY BACKENDS
+func (p *ReverseProxy) getNextBackend() *Backend {
+	n := len(p.backends)
+
+	for i := 0; i < n; i++ {
+		index := atomic.AddUint64(&p.current, 1) % uint64(n)
+		backend := p.backends[index]
+
+		if backend.Alive {
+			return backend
+		}
+	}
+
+	return nil
+}
+
 func (rp *ReverseProxy) serveHTTP(w http.ResponseWriter, r *http.Request) {
-	index := atomic.AddUint64(&rp.current, 1) % uint64(len(rp.backends))
-	proxy := httputil.NewSingleHostReverseProxy(rp.backends[index])
+	backend := rp.getNextBackend()
+	if backend == nil {
+		http.Error(w, "No available backend", http.StatusServiceUnavailable)
+		return
+	}
+	proxy := httputil.NewSingleHostReverseProxy(backend.URL)
 	proxy.ServeHTTP(w, r)
 }
 
 func (p *ReverseProxy) healthCheck() {
 	for {
 		for _, backend := range p.backends {
-			resp, err := http.Get(backend.String())
+			resp, err := http.Get(backend.URL.String())
 			if err != nil {
-				log.Printf("Error checking backend %s: %v", backend.String(), err)
-			} else {
-				resp.Body.Close()
+				log.Printf("Backend DOWN: %s", backend.URL)
+				backend.Alive = false
+				continue
 			}
+
+			resp.Body.Close()
+			backend.Alive = true
+			log.Printf("Backend UP: %s", backend.URL)
 		}
-		time.Sleep(10 * time.Second)
+
+		time.Sleep(5 * time.Second)
 	}
 }
 
